0d988549863196e6d52394d9d5bc76de5fddd27f
karlward <karlward@jupitercrash.net>
2012-10-27 16:42:35 -0400

    minor changes, added some comments, renamed some variables --KW

--- Wiring/serialcomm_simple/serialcomm_simple.ino
+++ Wiring/serialcomm_simple/serialcomm_simple.ino
@@ -2,8 +2,8 @@
 const int INVITATION = 127; // handshake invitation 
 boolean myTurn = true;
 int send = 1; 
-int newByte;
-int presence = 0; 
+int clutter = 0; // the amount of clutter, sent via serial FROM Processing
+int presence = 0; // whether someone is on the mat, sent via serial TO Processing
 
 void setup() {
   pinMode(13, OUTPUT); // for blink test 
@@ -37,13 +37,10 @@ void sendData() {
 }
 
 void readData() {
-  newByte = Serial.read();
-  if (newByte > 50) { 
-    digitalWrite(13, HIGH);
-  }
-  else { 
-    digitalWrite(13, LOW);
-  }
+  clutter = Serial.read();
+  // replace digitalWrite with servo code 
+  if (clutter > 50) { digitalWrite(13, HIGH); }
+  else { digitalWrite(13, LOW); }
   myTurn = true;
 }
 
