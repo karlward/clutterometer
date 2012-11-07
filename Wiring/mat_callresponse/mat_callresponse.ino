6bd51a9ea44b4c94dd1482c42df53ba9293fd4d7
Andrew Cerrito <andrewcerrito@gmail.com>
2012-11-07 10:19:30 -0500

    changed mat output to 0-255

--- Wiring/mat_callresponse/mat_callresponse.ino
+++ Wiring/mat_callresponse/mat_callresponse.ino
@@ -1,36 +1,27 @@
 
-int leftSensor, rightSensor;  // ints for left and right FSR
-
+int fsrValue;  // int to store FSR reading
+byte mappedValue; // FSR reading constrained to 0-255, safeguard against overflow
 void setup() {
   // open serial communications at 9600 bps
   Serial.begin(9600);
-  establishContact();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-     // read the incoming byte:
-    int inByte =Serial.read();
-    
-   // read the sensor:
-   leftSensor =analogRead(A0)/4;
-   // print the results:
-  Serial.print(leftSensor, DEC);
-  Serial.print(",");
-
-   // read the sensor:
-  rightSensor =analogRead(A1)/4;
-   // print the results:
-  Serial.println(rightSensor, DEC);
-   }
+  // read the sensor:
+  fsrValue = analogRead(A0)/4; // divide to 0-255 range
+  mappedValue = byte(constrain(fsrValue,0,255)); // extra measure against overflow
+//  if (fsrValue <=200) {
+//    mappedValue = 0;
+//   // Serial.write(0);
+//  } 
+//  else {
+//    mappedValue = 1;
+//    //Serial.write(1);
+//  }
+//  Serial.write(fsrValue);
+Serial.write(mappedValue);
 }
 
 
-void establishContact() {
- while (Serial.available() <= 0) {
-      Serial.println("hello");   // send a starting message
-      delay(300);
-   }
- }
 
 
